776cc9ca63da70991d759603e302af71200713a4
lego <lego@manque.DCC-PREGRADO>
2019-06-06 19:23:39 -0400

    Add ColorMapper.java

---
+++ src/ColorMapper.java
@@ -0,0 +1,51 @@
+import lejos.robotics.Color;
+
+
+public class ColorMapper {
+
+    Color[] colors;
+    String[] names;
+
+    public ColorMapper() {
+    }
+
+    public void setColors(Color[] colors) {
+        this.colors = colors;    
+    }
+
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+
+    public int getId(Color color) {
+        int min = 255 * 3; 
+        int id = -1;
+        for (int i = 0; i < colors.length; ++i) {
+            int diff = ColorUtils.difference(color, colors[i]);
+            if (diff < min) {
+                min = diff;
+                id = i;
+            }
+        }
+        return id;
+    }
+
+}
+
+class ColorUtils {
+
+    public static int norm(Color c) {
+        return (c.getRed() + c.getGreen() + c.getBlue());
+    }
+
+    public static Color colorDifference(Color a, Color b) {
+        return new Color(Math.abs(a.getRed() - b.getRed()),
+                Math.abs(a.getGreen() - b.getGreen()),
+                Math.abs(a.getBlue() - b.getBlue()));
+    }
+
+    public static int difference(Color a, Color b) {
+        return norm(colorDifference(a, b));
+    }
+
+}
